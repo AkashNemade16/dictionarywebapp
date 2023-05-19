568856ebd1cb8a442ebd8cdd519312daac472a70
akash <aksh.akash@gmail.com>
2023-05-18 22:03:01 -0500

    rendering data done

--- src/components/DictionayPage/DictionaryPage.js
+++ src/components/DictionayPage/DictionaryPage.js
@@ -1,20 +1,24 @@
-import { useEffect,useState,useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 
-import './DictionaryPage.sass'
-import {UserContext} from "../../UserContext/UserContext";
+import "./DictionaryPage.sass";
+import { UserContext } from "../../UserContext/UserContext";
 const DictionaryPage = () => {
-    const {userInput,apiData} = useContext(UserContext)
-    const [data] = apiData?.data.map((item)=>item.meanings) || []
-    const partOfSpeech = data?.map((item)=>item.partOfSpeech)
-    console.log(partOfSpeech)
-    console.log(data)
-
-    
-    return (
-        <div className="dictionary-page">
-            {data?.map((item)=><div>{item.partOfSpeech}</div>)}
+  const { userInput, apiData } = useContext(UserContext);
+  const [data] = apiData?.data.map((item) => item.meanings) || [];
+  console.log(data);
+  return (
+    <div className="dictionary-page">
+      {data?.map((item) => (
+        <div className="info">
+          <div className="part-of-speech">{item.partOfSpeech}</div>
+          {item.partOfSpeech && item.definitions
+            ? item.definitions.map((item) => <div className="definitions">{item.definition}</div>)
+            : null}
+          {item.synonyms && item.partOfSpeech ? item.synonyms.map((item)=><div className="synonyms">{item}</div>):null}
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default DictionaryPage
+export default DictionaryPage;
